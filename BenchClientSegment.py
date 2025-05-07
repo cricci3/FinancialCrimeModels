6f2e6888d7f26d764d1cb6d0d1cb2c7258e49b20
cricci3 <claudioricci.99@gmail.com>
2025-05-07 17:17:25 +0200

    fixed params in main

--- BenchClientSegment.py
+++ BenchClientSegment.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     df, name, dimension, account_prop = load_dataset()
 
     # Extract time series
-    extract_timeseries(df)
+    extract_timeseries(df, name)
 
     # Normalise time series
     Y_norm = normalization(df, name)
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     adj_matrix = adjaceny_matrix(Y_norm, name, dimension)
 
     # Run SQUIC_fit
-    W_matrices, _ = squic_fit_computation(Y_norm, name, dimension, adj_matrix)
+    W_matrices, _ = squic_fit_computation(Y_norm, name, dimension, adj_matrix, printMatrix=False)
     
     # Use the extracted W for clustering
     dict_cluster = clustering(W_matrices)
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     int_metrics = internal_metrics(dict_cluster, W_matrices)
     visualize_metrics(int_metrics)
 
-    # Visualise with cosmograph
+    # Visualise with cosmograph -> not working in .py file
     # visualize_graph(W_matrices, dict_cluster, account_prop, name, dimension)
--- BenchClientSegment_labels.py
+++ BenchClientSegment_labels.py
@@ -1,28 +1,31 @@
-from workflow.internal import extract_timeseries, normalization, squic_fit_computation, clustering, internal_metrics, visualize_metrics, visualize_graph
-from workflow.external import load_dataset, squic_computation, lda, external_metrics
+from workflow.internal import extract_timeseries, normalization, adjaceny_matrix, squic_fit_computation, clustering, internal_metrics, visualize_metrics, visualize_graph
+from workflow.external import load_dataset, squic_computation, linear_DA 
 
 
 if __name__ == '__main__':
     # Load dataset (the user will pass the name)
-    df, name, dimension = load_dataset()
+    df, name, dimension, account_prop = load_dataset()
 
     # Extract time series
-    extract_timeseries(df)
+    extract_timeseries(df, name)
 
     # Normalise time series
     Y_norm = normalization(df, name)
 
-    # Run SQUIC_fit to compute W
-    # W_matrices, _ = squic_fit_computation(Y_norm, name, dimension)
+    # Create transaction matrix
+    adj_matrix = adjaceny_matrix(Y_norm, name, dimension)
+
+    # Run SQUIC_fit to compute A (old W)
+    A_matrices, _ = squic_fit_computation(Y_norm, name, dimension, adj_matrix, printMatrix=False)
 
     # Run SQUIC to compute Θ
     Theta_matrices, _ = squic_computation(Y_norm, name, dimension)
 
     # Use the extracted W for clustering
-    # dict_cluster = clustering(W_matrices) # lambdas can be removed
+    dict_cluster = clustering(A_matrices)
 
     # extract Θ for LDA
-    dict_scores, y = lda(Theta_matrices, df)
+    dict_scores, y = linear_DA(Theta_matrices, account_prop)
 
     # Report internal metrics on the clustering
     # int_metrics = internal_metrics(dict_cluster, W_matrices)
@@ -32,7 +35,7 @@ if __name__ == '__main__':
     # visualize_graph(W_matrices, dict_cluster, name, dimension)
 
     # Report external metrics on the classification of Θ
-    ext_metrics = external_metrics(dict_scores, y)
-    print(ext_metrics)
+    #ext_metrics = external_metrics(dict_scores, y)
+    #print(ext_metrics)
 
     # Visualize with cosmograph
