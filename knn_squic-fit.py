18f69dc2f4176fbac1e93e157faf6a5f5ecce471
cricci3 <claudioricci.99@gmail.com>
2025-06-04 13:35:24 +0200

    knn squic fit

--- knn_squic-fit.py
+++ knn_squic-fit.py
@@ -1,4 +1,4 @@
-from workflow.internal import load_dataset, knn_graph, normalization, visualize_metrics
+from workflow.internal import load_dataset, knn_graph, normalization, visualize_metrics, extract_timeseries
 from workflow.SQUIC_functions import squic_fit_matrix_computation
 from workflow.clustering_functions import clustering_2_communities, modularity_fscore, plot_Q_f1, labels_to_partition, partition_to_labels
 from scipy.sparse import csr_matrix
@@ -19,28 +19,46 @@ if __name__ == '__main__':
     Y, name, dimension, account_prop, trans_matrix = load_dataset()
 
     # Extract time series
-    # extract_timeseries(Y, name)
+    extract_timeseries(Y, name)
 
     # Normalise time series
     Y_norm = normalization(Y, name)
 
-    n_neighbors = 10
+    n_neighbors = 20
     nbrs = NearestNeighbors(n_neighbors=n_neighbors + 1, metric='euclidean', n_jobs=-1)
     nbrs.fit(Y_norm)
     knn_matrix = nbrs.kneighbors_graph(Y_norm, mode='connectivity')
 
     G_knn = nx.from_scipy_sparse_array(knn_matrix)
 
-    print("++++++++++++++++++++++++")
-    print(f"Graph given by KNN on timeseries is connected? {nx.is_connected(G_knn)}")
-    print("++++++++++++++++++++++++")
+    print("++++++++++++++++++++++++++++++++++++++++++++++")
+    if nx.is_connected(G_knn):
+        print(f"Graph given by KNN on timeseries is connected")
+    else:
+        components = list(nx.connected_components(G_knn))
+        main_component = max(components, key=len)
+        isolated_nodes = [list(comp)[0] for comp in components if len(comp) == 1]
+        
+        print(f"Main component: {len(main_component)} nodes")
+        print(f"Isolated nodes: {len(isolated_nodes)} nodes")
+        print(f"Other components: {len(components) - len(isolated_nodes) - 1}\n")
+    print("++++++++++++++++++++++++++++++++++++++++++++++")
+
+    # Print knn matrix
+    plt.figure(figsize=(7, 7))
+    plt.spy(knn_matrix, markersize=5)
+    plt.ylabel("Users", fontsize=18)
+
+    plt.tick_params(axis='x')
+    plt.tick_params(axis='y') 
+    plt.show()
 
     results_squic = {}
 
     # Run SQUIC_fit
-    results_squic['squic-fit-matrix'] = squic_fit_matrix_computation(Y_norm, name, dimension, knn_matrix, printMatrix=False)
+    results_squic['squic-fit-matrix'] = squic_fit_matrix_computation(Y_norm, name, dimension, knn_matrix, printMatrix=True)
 
-    dict_cluster = clustering_2_communities(results_squic, name, account_prop)
+    dict_cluster = clustering_2_communities(results_squic, method='implemented')
 
     metrics = modularity_fscore(dict_cluster, results_squic, account_prop)
 
