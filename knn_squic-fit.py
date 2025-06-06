9857f2fe387c61d26543d8df5491732f5020dfa2
cricci3 <claudioricci.99@gmail.com>
2025-06-06 10:25:55 +0200

    save data

--- knn_squic-fit.py
+++ knn_squic-fit.py
@@ -3,39 +3,84 @@ from workflow.SQUIC_functions import squic_fit_matrix_computation, squic_fit_com
 from workflow.clustering_functions import clustering_2_communities, modularity_fscore, ARI_fscore, plot_ARI_f1, labels_to_partition, partition_to_labels
 import matplotlib.pyplot as plt
 import networkx as nx
-
+import json
+from scipy import sparse
 from sklearn.neighbors import NearestNeighbors
+import pandas as pd
 
 
 if __name__ == '__main__':
-    # Load dataset (the user will pass the name)
-    Y, name, dimension, account_prop, trans_matrix = load_dataset()
+    user_input = input("Do you want to load data? (Y/N)")
+    user_input = user_input.upper()
 
-    # Extract time series
-    extract_timeseries(Y, name)
+    if user_input == 'Y':
+        dimension = input("Which dimension?")
+        dimension = dimension.upper()
 
-    # Normalise time series
-    Y_norm = normalization(Y, name)
+        path = 'paysim_data_saved'
 
-    n_neighbors = 7
-    nbrs = NearestNeighbors(n_neighbors=n_neighbors + 1, metric='euclidean', n_jobs=-1)
-    nbrs.fit(Y_norm)
-    knn_matrix = nbrs.kneighbors_graph(Y_norm, mode='connectivity') # sparse matrix
+        try:
+            # Set the chunk size (e.g., 100,000 rows per chunk)
+            chunk_size = 1000
 
-    G_knn = nx.from_scipy_sparse_array(knn_matrix)
+            chunks = []
 
-    print("++++++++++++++++++++++++++++++++++++++++++++++")
-    if nx.is_connected(G_knn):
-        print(f"Graph given by KNN on timeseries is connected")
-    else:
-        components = list(nx.connected_components(G_knn))
-        main_component = max(components, key=len)
-        isolated_nodes = [list(comp)[0] for comp in components if len(comp) == 1]
+            # Read CSV in chunks
+            for chunk in pd.read_csv(f'{path}/YNorm_{dimension}.csv', chunksize=chunk_size):
+                chunks.append(chunk)
+
+            # Concatenate all chunks into a single DataFrame
+            Y_norm = pd.concat(chunks, ignore_index=True)
+
+            knn_matrix = sparse.load_npz(f'{path}/knn_matrix_{dimension}.npz')
+
+            print(f"Shape of Y_norm loaded: {Y_norm.shape}")
+            print(f"Shape of knn_matrix loaded: {knn_matrix.shape}")
+            print(f"Type of knn_matrix loaded: {type(knn_matrix)}")
+
+            with open(f'{path}/account_prop_{dimension}.json', 'r') as f:
+                account_prop = json.load(f)
+
+            name = 'PAYSIM'
+        except:
+            print(f"No saved data found for dimension {dimension}")
         
-        print(f"Main component: {len(main_component)} nodes")
-        print(f"Isolated nodes: {len(isolated_nodes)} nodes")
-        print(f"Other components: {len(components) - len(isolated_nodes) - 1}\n")
-    print("++++++++++++++++++++++++++++++++++++++++++++++")
+    else:
+        # Load dataset (the user will pass the name)
+        Y, name, dimension, account_prop, _ = load_dataset()
+
+        # Extract time series
+        # extract_timeseries(Y, name)
+
+        # Normalise time series
+        Y_norm = normalization(Y, name)
+
+        print(Y_norm.shape)
+
+        n_neighbors = 3
+        nbrs = NearestNeighbors(n_neighbors=n_neighbors + 1, metric='euclidean', n_jobs=-1)
+        nbrs.fit(Y_norm)
+        knn_matrix = nbrs.kneighbors_graph(Y_norm, mode='connectivity') # sparse matrix
+
+        user_input = input("Do you want to save this data? (Y/N)")
+        user_input = user_input.upper()
+
+        if user_input == 'Y':
+            # Save to fast init
+            path = 'paysim_data_saved'
+
+            # Save Y norm to csv
+            # Y_norm is rows = users, col = days
+            df_save = pd.DataFrame(Y_norm)
+            df_save.to_csv(f'{path}/YNorm_{dimension}.csv', index=False)
+
+            # Save account prop as JSON
+            with open(f'{path}/account_prop_{dimension}.json', 'w') as f:
+                json.dump(account_prop, f)
+
+            # Save knn matrix
+            sparse.save_npz(f'{path}/knn_matrix_{dimension}.npz', knn_matrix)
+
 
     # Print knn matrix
     plt.figure(figsize=(7, 7))
