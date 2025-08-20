fdaf03ca8aa2441fec3a1bc0f2efb6fc69ff993b
cricci3 <claudioricci.99@gmail.com>
2025-08-20 10:02:49 +0200

    visualize G w/out clusters

--- functions/cosmograph_functions.py
+++ functions/cosmograph_functions.py
@@ -259,132 +259,121 @@ def visualize_graph_amlsim(W_matrices, squic_method, dict_partition, account_pro
     
     return widget_dict
 
-# def visualize_graph(W_matrices, squic_method, account_prop, name, two_comm=True):
 
-#     widget_dict = {
-#         rho: {} for rho in W_matrices[squic_method].keys()
-#     }
+def cosmograph_decomposition(widget_dict, theta, rho, dict_partition, color_list=COLOR_LIST):
+
+    widget_dict[rho] = {
+            'louvain' : {},
+            'leiden' : {},
+            'dbscan' : {},
+            'spectral' : {}
+    }
+
     
-        
-#     for rho, X in W_matrices[squic_method].items():
-#         # Create a graph from matrix X
-#         G = nx.from_numpy_array(X)
+    for clustering_method, partition in dict_partition.items():
+        # Create a graph from matrix X
+        G = nx.from_numpy_array(theta)
 
-#         fraud_mapping = {}
-#         class_mapping = {}
-#         for node in G.nodes():
-#             if node in account_prop and isinstance(account_prop[node], dict):
-#                 if 'fraud' in account_prop[node]:
-#                     fraud_mapping[node] = account_prop[node]['fraud']
-#                 if name == 'PAYSIM' and 'class' in account_prop[node]:
-#                     class_mapping[node] = account_prop[node]['class']
-#             else:
-#                 # Default value if node not found in account_prop
-#                 fraud_mapping[node] = False
-        
-#         # Set fraud attributes for all nodes
-#         nx.set_node_attributes(G, fraud_mapping, 'fraud')
-        
-#         if name == 'PAYSIM':
-#             nx.set_node_attributes(G, class_mapping, 'class')
+        if partition != None:
+            community_mapping = node_to_community(partition)
 
-#         # Nodes: build dataframe from node attributes
-#         nodes_data = []
-#         fraudolent = []
+            # Sets node attributes from a given value or dictionary of values.
+            nx.set_node_attributes(G, community_mapping, 'community')        
 
-#         for node, data in G.nodes(data=True):
-#             is_fraud = data.get('fraud', False)
-#             if name == 'PAYSIM':
-#                 user_class = data.get('class', False)
+            # Nodes: build dataframe from node attributes
+            nodes_data = []
 
-#             # Create conditional label - only show label ID if fraudulent
-#             label = str(node) if is_fraud else ""
+            for node, data in G.nodes(data=True):
+                nodes_data.append({
+                    'id': node,
+                    'community': data.get('community'),
+                    'color': data.get('color'),
+                    'degree': G.degree[node],
+                })
+            
+            points = pd.DataFrame(nodes_data)
+            points['community'] = points['community'].astype('category')
 
-#             if is_fraud:
-#                 fraudolent.append(str(node))
+            # Edges: extract links with weights
+            links_data = []
+            for u, v, d in G.edges(data=True):
+                links_data.append({
+                    'source': u,
+                    'target': v,
+                    'weight': d.get('weight', 1.0)
+                })
+            links = pd.DataFrame(links_data)
+            links['weight'] = links['weight'].abs()  # only positive thickness
+            # links['weight'] = links['weight'] * 10  # only positive thickness
 
-#             if name != 'PAYSIM':
-#                 nodes_data.append({
-#                     'id': node,
-#                     # 'label': str(node),
-#                     'label' : label,
-#                     'community': data.get('community'),
-#                     'color': data.get('color'),
-#                     'degree': G.degree[node],
-#                     'fraud' : is_fraud,
-#                 })
-#             else:
-#                 nodes_data.append({
-#                 'id': node,
-#                 # 'label': str(node),
-#                 'label' : label,
-#                 'community': data.get('community'),
-#                 'color': data.get('color'),
-#                 'degree': G.degree[node],
-#                 'fraud' : is_fraud,
-#                 'class' : user_class
-#                 })
-        
-#         points = pd.DataFrame(nodes_data)
+            # COSMOGRAPH docs: https://colab.research.google.com/drive/1Rt8rmmeMuWyFjEqae2DdJ3NYymtjC9cT#scrollTo=IZUK7ioL1xKr
+            widget = cosmo(
+                points=points,
+                links=links,
+                link_source_by='source',
+                link_target_by='target',
+                link_width_by='weight', # width of an edge given by weight = value in X between i and j
+                link_color='#E3E3E3',
+                link_greyout_opacity=0.1,
+                point_color_by='community',
+                point_size_by='degree',
+                point_color_strategy='map',
+                point_color_by_map=color_list,
+                background_color='#FFFFFF',
+            )
 
-#         points['community'] = points['community'].astype('category')
+            widget_dict[rho][clustering_method] = widget
 
-#         # Edges: extract links with weights
-#         links_data = []
-#         for u, v, d in G.edges(data=True):
-#             links_data.append({
-#                 'source': u,
-#                 'target': v,
-#                 'weight': d.get('weight', 1.0)
-#             })
-#         links = pd.DataFrame(links_data)
-#         links['weight'] = links['weight'].abs()  # only positive thickness
-#         # links['weight'] = links['weight'] * 10  # only positive thickness
-        
-#         if two_comm == True:
-#             color_list = [
-#                 ["0", "#90ee90"],  # 'Clients': 'mediumseagreen'
-#                 ["1", "#00CED1"],  # 'Merchants': 'darkturquoise'
-#             ]
-#         else:
-#             color_list = [
-#                 ["0", "#4682B4"],  # Steelblue
-#                 ["1", "#FF9999"],  # Red/Pink
-#                 ["2", "#4169E1"],  # Royal Blue
-#                 ["3", "#FFD700"],  # Yellow
-#                 ["4", "#FFCC99"],  # Orange
-#                 ["5", "#CCFFE5"],  # Light blue
-#                 ["6", "#FFFF99"],  # Yellow
-#                 ["7", "#CCFF99"],  # Lime
-#                 ["8", "#CCFFFF"],  # Cyan
-#                 ["9", "#CCCCFF"],  # Lilla
-#                 ["10", "#E9967A"],  # Dark Salmon
-#                 ["11", "#E5CCFF"],  # Dark Lilla
-#                 ["12", "#DC143C"], # Crimson Red
-#                 ["13", "#FFFFFF"], # White
-#                 ["14", "#F0FFF0"],  # Honeydew,
-#             ]
-
-#         # COSMOGRAPH docs: https://colab.research.google.com/drive/1Rt8rmmeMuWyFjEqae2DdJ3NYymtjC9cT#scrollTo=IZUK7ioL1xKr
-#         widget = cosmo(
-#             points=points,
-#             links=links,
-#             point_id_by='id', # id
-#             link_source_by='source',
-#             link_target_by='target',
-#             link_width_by='weight', # width of an edge given by weight = value in X between i and j
-#             link_color='#E3E3E3',
-#             link_greyout_opacity=0.1,
-#             # link_color_by='community',
-#             point_color_by='class',
-#             # point_label_by='class', # or id or fraud 
-#             point_size_by='degree', #'degree
-#             point_color_strategy='map',
-#             point_color_by_map=color_list,
-#             background_color='#FFFFFF',
-#             show_labels_for=fraudolent
-#         )
-
-#         widget_dict[rho] = widget
+        else:
+            print(f"No partition for rho {rho}, {clustering_method}")
+            widget_dict[rho][clustering_method] = "No clustering"
     
-#     return widget_dict
+    return widget_dict
+
+
+def cosmograph_from_theta(theta, rho, widget_dict):    
+    # Create a graph from matrix X
+    G = nx.from_scipy_sparse_array(theta)
+
+    nodes_data = []
+
+    for node, data in G.nodes(data=True):
+        nodes_data.append({
+            'id': node,
+            'degree': G.degree[node],
+        })
+            
+    points = pd.DataFrame(nodes_data)
+
+    # Edges: extract links with weights
+    links_data = []
+    for u, v, d in G.edges(data=True):
+        links_data.append({
+            'source': u,
+            'target': v,
+            'weight': d.get('weight', 1.0)
+        })
+    links = pd.DataFrame(links_data)
+    links['weight'] = links['weight'].abs()  # only positive thickness
+    # links['weight'] = links['weight'] * 10  # only positive thickness
+
+    # COSMOGRAPH docs: https://colab.research.google.com/drive/1Rt8rmmeMuWyFjEqae2DdJ3NYymtjC9cT#scrollTo=IZUK7ioL1xKr
+    widget = cosmo(
+        points=points,
+        links=links,
+        point_id_by=None,
+        link_source_by='source',
+        link_target_by='target',
+        link_width_by='weight',
+        link_color='#E3E3E3',
+        link_greyout_opacity=0.1,
+        # link_color_by='community',
+        # point_color='lightskyblue',
+        point_color='#0d5da4',
+        point_size_by='degree',
+        background_color='#FFFFFF',
+    )
+
+    widget_dict[rho] = widget
+    
+    return widget_dict
